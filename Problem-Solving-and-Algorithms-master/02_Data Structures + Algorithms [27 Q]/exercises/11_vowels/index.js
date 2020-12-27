7dfc467663a58389c5244faa2f39509b1ad3bdfb
aihamjarrah <ayham.mjarr@gmail.com>
2020-12-27 15:26:12 +0200

    Solved Q11

--- Problem-Solving-and-Algorithms-master/02_Data Structures + Algorithms [27 Q]/exercises/11_vowels/index.js
+++ Problem-Solving-and-Algorithms-master/02_Data Structures + Algorithms [27 Q]/exercises/11_vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowelLetters = "aeiou";
+    let counter =0 ;
+    str = str.toLowerCase()
+    const splitted = str.split('');
+    splitted.forEach((e, i) => {
+        if(vowelLetters.includes(e)){
+            counter+=1
+        }
+    })
+    return counter
+}
 
 module.exports = vowels;
