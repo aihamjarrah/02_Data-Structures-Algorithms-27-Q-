3d520bbefe737e068bbc455d01deb37c8f5b1b63
aihamjarrah <ayham.mjarr@gmail.com>
2020-12-27 02:07:31 +0200

    Solved Q4

--- Problem-Solving-and-Algorithms-master/02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ Problem-Solving-and-Algorithms-master/02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charachters = {};
+    let counter = 0;
+    let mostUsedChar = ''
+    strSplitted = str.split('');
+    strSplitted.forEach((e, i) => {
+        if(charachters[e] === undefined){
+            charachters[e] = 1
+            
+        }
+        else{
+            charachters[e] +=1
+            
+        }
+    });
+    for(key in charachters){
+        if(charachters[key] > counter){
+            counter = charachters[key]
+            mostUsedChar = key
+
+        }
+
+    }
+    return mostUsedChar
+}
 
 module.exports = maxChar;
